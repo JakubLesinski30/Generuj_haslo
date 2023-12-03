fa1e7e16db3ed15f5a36f5d2bd37110b23cf87ce
JakubLesinski30 <jakub.lesinski30@gmail.com>
2023-12-03 17:59:56 +0100

    Generowanie hasła v2

--- src/Generuj.java
+++ src/Generuj.java
@@ -1,66 +1,110 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.security.SecureRandom;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 public class Generuj extends JFrame {
 
     private static final String ZNAKI = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=[{]}<>.?/!@#$%^&*()_+";
 
+    private JButton generujButton;
+    private JPanel passwordsPanel;
+
     public Generuj() {
         setTitle("Generator Haseł");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 320);
+        setSize(800, 600);
         setLocationRelativeTo(null);
+
         JPanel panel = new JPanel(new BorderLayout());
         panel.setBackground(Color.decode("#FFECB3"));
-        JTextArea textArea = new JTextArea();
-        textArea.setEditable(false);
-        textArea.setBackground(Color.decode("#FFECB3"));
-        JScrollPane scrollPane = new JScrollPane(textArea);
 
-        JButton generujButton = new JButton("Generuj Ponownie");
+        passwordsPanel = new JPanel();
+        passwordsPanel.setLayout(new BoxLayout(passwordsPanel, BoxLayout.Y_AXIS));
+        passwordsPanel.setBackground(Color.decode("#FFECB3"));
+        JScrollPane scrollPane = new JScrollPane(passwordsPanel);
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setBackground(Color.decode("#FFECB3"));
+
+        JLabel labelIloscHasel = new JLabel("Ilość haseł:");
+        JTextField fieldIloscHasel = new JTextField(5);
+
+        JLabel labelDlugoscHasla = new JLabel("Długość hasła:");
+        JTextField fieldDlugoscHasla = new JTextField(5);
+
+        generujButton = new JButton("Generuj");
         generujButton.setBackground(Color.decode("#536DFE"));
-        generujButton.setForeground(Color.BLACK);
+        generujButton.setForeground(Color.WHITE);
         generujButton.setFont(new Font("Arial", Font.BOLD, 18));
 
-        generujButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                generujIPokazHasla(textArea);
+        generujButton.addActionListener(e -> {
+            try {
+                int iloscHasel = Integer.parseInt(fieldIloscHasel.getText());
+                int dlugoscHasla = Integer.parseInt(fieldDlugoscHasla.getText());
+                generujIPokazHasla(passwordsPanel, iloscHasel, dlugoscHasla);
+                generujButton.setText("Generuj Ponownie");
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Wprowadź poprawne liczby", "Błąd", JOptionPane.ERROR_MESSAGE);
             }
         });
 
+        inputPanel.add(labelIloscHasel);
+        inputPanel.add(fieldIloscHasel);
+        inputPanel.add(labelDlugoscHasla);
+        inputPanel.add(fieldDlugoscHasla);
+
+        panel.add(inputPanel, BorderLayout.NORTH);
         panel.add(scrollPane, BorderLayout.CENTER);
         panel.add(generujButton, BorderLayout.SOUTH);
 
         add(panel, BorderLayout.CENTER);
-
-        generujIPokazHasla(textArea);
     }
 
-    private void generujIPokazHasla(JTextArea textArea) {
-        int iloscHasel = 11;
-        int dlugoscHasla = 12;
-    
-        textArea.setText("");
+    private void generujIPokazHasla(JPanel passwordsPanel, int iloscHasel, int dlugoscHasla) {
+        passwordsPanel.removeAll();
+        passwordsPanel.revalidate();
+        passwordsPanel.repaint();
+
         for (int i = 0; i < iloscHasel; i++) {
             String wygenerowaneHaslo = generujHaslo(dlugoscHasla);
-            textArea.append(wygenerowaneHaslo);
-            if (i < iloscHasel - 1) {
-                textArea.append("\n");
-            }
+
+            JPanel rowPanel = new JPanel(new BorderLayout());
+            rowPanel.setBackground(Color.decode("#FFECB3"));
+
+            JTextArea hasloTextArea = new JTextArea(wygenerowaneHaslo);
+            hasloTextArea.setEditable(false);
+            hasloTextArea.setBackground(Color.decode("#FFECB3"));
+            hasloTextArea.setLineWrap(true);
+
+            JButton kopiujButton = new JButton("Kopiuj");
+            kopiujButton.setBackground(Color.BLACK);
+            kopiujButton.setForeground(Color.WHITE);
+
+            kopiujButton.addActionListener(e -> skopiujDoSchowka(wygenerowaneHaslo));
+
+            JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+            buttonPanel.setBackground(Color.decode("#FFECB3"));
+            buttonPanel.add(kopiujButton);
+
+            rowPanel.add(hasloTextArea, BorderLayout.CENTER);
+            rowPanel.add(buttonPanel, BorderLayout.EAST);
+
+            passwordsPanel.add(rowPanel);
         }
-    
-        Font font = new Font("Arial", Font.BOLD, 17);
-        textArea.setFont(font);
+
+        revalidate();
+        repaint();
+    }
+
+    private void skopiujDoSchowka(String tekst) {
+        StringSelection stringSelection = new StringSelection(tekst);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+        JOptionPane.showMessageDialog(null, "Skopiowano do schowka: " + tekst);
     }
 
     private String generujHaslo(int dlugosc) {
@@ -77,12 +121,10 @@ public class Generuj extends JFrame {
     }
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                Generuj generuj = new Generuj();
-                generuj.setResizable(false);
-                generuj.setVisible(true);
-            }
+        SwingUtilities.invokeLater(() -> {
+            Generuj generuj = new Generuj();
+            generuj.setResizable(false);
+            generuj.setVisible(true);
         });
     }
 }
